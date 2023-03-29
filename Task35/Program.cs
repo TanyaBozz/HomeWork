1a9f37076ff536a09bc4b107cedcbb1d68e53f43
Tanya Bozhok <lady.bozho4ek@gmail.com>
2023-03-29 13:18:42 +0300

    Выполнено решение задачи 35

---
+++ Task35/Program.cs
@@ -0,0 +1,49 @@
+﻿// 35. Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] arr = CreateArrayRndInt(123, -30, 30);
+PrintArray(arr);
+int count = CountElements(arr, 10, 99);
+Console.WriteLine($" -> {count}");
+
+int CountElements(int[] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= min && array[i] <= max)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
