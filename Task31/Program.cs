211959286f681f721081a7d82b05cedd90a62361
Tanya Bozhok <lady.bozho4ek@gmail.com>
2023-03-29 12:46:31 +0300

    Выполнено форматирование кода решения задачи 33

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -14,19 +14,19 @@ int[] CreateArrayRndInt(int size, int min, int max)
 
     for (int i = 0; i < arr.Length; i++)
     {
-        arr [i] = rnd.Next(min, max + 1);
+        arr[i] = rnd.Next(min, max + 1);
     }
     return arr;
-} 
+}
 
-void PrintArray (int[] arr)
+void PrintArray(int[] arr)
 {
     Console.Write("[");
-  for (int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-       if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-       else Console.Write($"{arr[i]}");
-    }  
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
     Console.Write("]");
 }
 
@@ -36,7 +36,7 @@ int SumNegativeElements(int[] arr)
     int sum = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-      if(arr[i] < 0) sum += arr[i];  
+        if (arr[i] < 0) sum += arr[i];
     }
     return sum;
 }
@@ -47,14 +47,14 @@ int SumPositiveElements(int[] arr)
     int sum = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-      if(arr[i] > 0) sum += arr[i];  
+        if (arr[i] > 0) sum += arr[i];
     }
     return sum;
 }
 
 
 
-int [] array = CreateArrayRndInt(12, -9, 9);
+int[] array = CreateArrayRndInt(12, -9, 9);
 PrintArray(array);
 Console.WriteLine();
 int sumNegativeElem = SumNegativeElements(array);
