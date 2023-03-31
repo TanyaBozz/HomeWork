16b90a3d497ffeabca0a4549a858f57ae83fb1c6
Tanya Bozhok <lady.bozho4ek@gmail.com>
2023-03-31 18:48:02 +0300

    Выполнено решение задачи 49

---
+++ Task49/Program.cs
@@ -0,0 +1,49 @@
+﻿// 49. Задайте двумерный массив. 
+// Найдите элементы, укоторых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"{i}->");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+void SquareNumber(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i = i + 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j = j + 2)
+        {
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+SquareNumber(array2d);
+PrintMatrix(array2d);
